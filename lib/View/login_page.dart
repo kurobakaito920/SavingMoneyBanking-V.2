1aa41fb58ce858d0760f17d293f20a55b2e7fd14
Sliance <sliance2402@gmail.com>
2023-06-14 18:53:00 +0700

    init project

---
+++ lib/View/login_page.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  Color hexToColor(String code) => new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: hexToColor('#f3f3f3'),
+        body:Stack(
+          children:<Widget> [
+            Positioned.fill(
+              child: Image.asset(
+                'images/background_login.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              color: Colors.transparent,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 8.0, top: 122.0),
+                      child: Image.asset(
+                        'images/logo.png',
+                        height: 87,
+                        width: 87,
+                      ),
+                    ),
+                    const Text(
+                      'SỔ TIẾT KIỆM',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2050C0),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 46.0, bottom: 14.0),
+                      child: SizedBox(
+                        width: 228,
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Tên người dùng',
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 14),
+                      child: SizedBox(
+                        width: 228,
+                        height: 40,
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Mật khẩu',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 79.0, bottom: 14),
+                        child: GestureDetector(
+                          onTap: (){},
+                          child: const Text(
+                            'Quên mật khẩu?',
+                            style: TextStyle(
+                              color: Color(0xFF587CD0),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 41,
+                      width: 179,
+                      child: ElevatedButton(
+                        onPressed: (){}, 
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF1E70C3),
+                        ),
+                        child: const Text(
+                          'ĐĂNG NHẬP',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ), 
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 560, bottom: 18),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Expanded(
+                    child: Divider(
+                      indent: 82.3,
+                      endIndent: 10,
+                      thickness: 2,
+                      color: Color(0xFF707070),
+                    ),
+                  ),
+                  Text(
+                    'Đăng nhập bằng',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0x80000000),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      indent: 10,
+                      endIndent: 82.3,
+                      thickness: 2,
+                      color: Color(0xFF707070),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:<Widget> [
+                Padding(
+                  padding: EdgeInsets.only(right: 22.5, left: 22.5),
+                ),
+              ],
+            ),
+          ],
+        ), 
+      ),
+    );
+  }
+}
+
